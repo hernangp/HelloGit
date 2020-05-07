287d3bba2ca8606e4e7c70d8d398361ea7376250
hernangp <47035008+hernangp@users.noreply.github.com>
2020-05-07 16:19:28 -0300

    Update speak.py

--- helloGit/speak.py
+++ helloGit/speak.py
@@ -1,6 +1,10 @@
 class talk(object):
-    def __init__(self,name):
+    def __init__(self,name, age=26):
         self.name = name
+        self.age = age
+        
+    def age(self):
+        print("I'm %s years old, i'm young, you are old" %age)
 
     def hi(self):
         print("Hello", self.name,". Have a good day." )
